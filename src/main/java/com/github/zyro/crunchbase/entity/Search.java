a19dbfb383fbdb5f2b9bb3d9427be5af2eb47634
Andrei Mihu <andrei@pieceof8.com>
2013-09-01 11:15:04 +0100

    Search entities.

---
+++ src/main/java/com/github/zyro/crunchbase/entity/Search.java
@@ -0,0 +1,54 @@
+package com.github.zyro.crunchbase.entity;
+
+import java.util.List;
+
+public class Search {
+
+    private Integer total;
+    private Integer page;
+    private String crunchbase_url;
+    private List<Result> results;
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public String getCrunchbase_url() {
+        return crunchbase_url;
+    }
+
+    public void setCrunchbase_url(String crunchbase_url) {
+        this.crunchbase_url = crunchbase_url;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    @Override
+    public String toString() {
+        return "Search{" +
+                "total=" + total +
+                ", page=" + page +
+                ", crunchbase_url='" + crunchbase_url + '\'' +
+                ", results=" + results +
+                '}';
+    }
+
+}
